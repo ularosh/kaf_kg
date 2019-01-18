7c1f0ac6a05186e64ca2c028411576de466bbe6e
ularosh <ular_osh@list.ru>
2019-01-18 20:13:37 +0600

    Маалымат кошулду

--- application/views/contact_view.php
+++ application/views/contact_view.php
@@ -2,7 +2,8 @@
 <div class="contact">
 	<div class="contact-top">
 		<h3>Contact Us</h3>
-		<p> Ош шаары .</p>
+		<p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+		Ош шаары .</p>
 	</div>
 	<div class="contact-bottom">
             <form action="<?php echo base_url('page/add_massange');?>" method="POST">
